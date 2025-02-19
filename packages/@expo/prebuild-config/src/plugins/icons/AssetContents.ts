cd7def9f185222ba5e80f3c2ce477e26c076e2e6
Phil Pluckthun <phil@kitten.sh>
2025-02-19 12:47:58 +0000

    chore(prebuild-config): Replace fs-extra in src/plugins/icons/AssetContents.ts

--- packages/@expo/prebuild-config/src/plugins/icons/AssetContents.ts
+++ packages/@expo/prebuild-config/src/plugins/icons/AssetContents.ts
@@ -1,4 +1,4 @@
-import fs from 'fs-extra';
+import fs from 'fs';
 import { join } from 'path';
 
 export type ContentsJsonImageIdiom =
@@ -64,9 +64,11 @@ export async function writeContentsJsonAsync(
   directory: string,
   { images }: Pick<ContentsJson, 'images'>
 ): Promise<void> {
-  await fs.ensureDir(directory);
+  if (!fs.existsSync(directory)) {
+    await fs.promises.mkdir(directory, { recursive: true });
+  }
 
-  await fs.writeFile(
+  await fs.promises.writeFile(
     join(directory, 'Contents.json'),
     JSON.stringify(
       {
